d8e9e27756b9f393e2d61f562e2b6b6bc734b956
satoshi <s@n.com>
2022-07-12 08:35:58 -0500

    Adding delete buttons at the last column

--- quotes/views.py
+++ quotes/views.py
@@ -1,8 +1,10 @@
 #from email import message
+from termios import TIOCPKT_FLUSHREAD
 from django.shortcuts import render, redirect
 from .models import Stock
 from .forms import StockForm
 from django.contrib import messages
+from django.template.defaulttags import register
 
 # Create your views here.
 # pk_40f41830cd17488cb06e00eebec9b2d6
@@ -31,7 +33,7 @@ def about(request):
 def add_stock(request):
     import requests
     import json
-
+    var1 = 1
     if request.method == 'POST':
         form = StockForm(request.POST or None)
 
@@ -52,7 +54,12 @@ def add_stock(request):
 
             except Exception as e:
                 api = "Error..."
-        return render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
+                
+        str_list = []
+        for i in ticker:
+            str_list.append(str(i))
+
+        return render(request, 'add_stock.html', {'ticker': ticker, 'output': output, 'range': range(0,len(ticker)), 'str_list': str_list})
 
 def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
@@ -63,3 +70,8 @@ def delete(request, stock_id):
 def delete_stock(request):
     ticker = Stock.objects.all()
     return render(request, 'delete_stock.html', {'ticker': ticker})
+
+@register.filter
+def index(indexable, i):
+    return indexable[i]
+
